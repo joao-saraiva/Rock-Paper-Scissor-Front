79faaaff2f5eb15347c9d1b6414b846fd4bb5d0f
joão <joao_saraiva@edu.unifor.br>
2022-05-11 19:48:12 -0300

    conclued the communication with back-end, incrementing score, and reseting thhe game

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const socket = io('http://localhost:3000');
+var score = 0;
+
 $('.versus-section').hide();
 
 socket.on('receivedOption', (data) =>{
-    console.log(data);
+    if(data.id != socket.id){
+        $('.enemy-pick').append(`<img src="icon-${data.option}.svg" alt="rock" class="choice text-center enemy-last-pick" id="${data.option}">`)
+    }
 })
 
 socket.on('alreadyChoosed', (data) => {
@@ -10,12 +14,28 @@ socket.on('alreadyChoosed', (data) => {
 })
 
 socket.on('result', (data) => {
-    alert(data.result);
+    if(data.result != 'draw'){
+        if(data.winnerId == socket.id){
+            console.log('You Win');
+            score += 1;
+        }else{
+            console.log('You Lose');
+        }
+    }
+
+    setTimeout(function(){
+        $('.buttons-section').show();
+        $('.versus-section').hide();
+        $('.actual-score').text(score);
+        $( ".enemy-last-pick" ).remove();
+        $( ".player-last-pick" ).remove();
+    }, 3000);
 })
 
-$('.choice').on('click', function() {
+$('.select-choice').on('click', function() {
     socket.emit('selectedOption', {option: $(this).attr('id')});
-    $('.current-player-pick').append(`<img src="icon-${$(this).attr('id')}.svg" alt="rock" class="choice text-center" id="${$(this).attr('id')}">`)
+    $('.current-player-pick').append(`<img src="icon-${$(this).attr('id')}.svg" alt="rock" class="choice text-center player-last-pick" id="${$(this).attr('id')}">`)
     $('.buttons-section').hide();
     $('.versus-section').show();
 })
+
