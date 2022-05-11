322ab81b7492d9e86a4077138a04c208f42903da
João <programador09@speedgov.com.br>
2022-05-10 21:46:28 -0300

    using primary libraries

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-axios.get('http://localhost:3000/teste')
-    .then(response => console.log(response.data))
-        .catch(err => console.log(err))
+
+
 Array.from(document.getElementsByClassName("choice")).forEach(function(element){
     element.addEventListener("click", function(e){
-        console.log(e.srcElement.id)
+        axios.post('http://localhost:3000/choices/new', {nome: e.srcElement.id})
+            .then(response => console.log(response.data))
+                .catch(err => console.log(err))
     })
+})
+
+const sendChoice = document.getElementById('send-choice');
+
+sendChoice.addEventListener("click", function(){
+    axios.post('http://localhost:3000/rounds', {nome: "teste"})
+            .then(response => console.log(response.data))
+                .catch(err => console.log(err))
 })
